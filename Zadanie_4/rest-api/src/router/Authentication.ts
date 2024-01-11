5358dd469b200e14d749c94725d27d78dd622131
242447 Kwiatkowski Piotr <242447@edu.p.lodz.pl>
2024-01-11 16:27:14 +0100

    Added morgan and some types dependencies to package.json since project structure changed. Also changed authentication so that all accounts can be created without authorization (to be changed later).

--- Zadanie_4/rest-api/src/router/Authentication.ts
+++ Zadanie_4/rest-api/src/router/Authentication.ts
@@ -7,8 +7,10 @@ import { userImageMiddleware } from '../middlewares/ImageUpload';
 
 export default(router : express.Router) => {
     router.post('/users/signup', userImageMiddleware, createClient);
-    router.post('/users/staff', userImageMiddleware, checkRolesPermission(new Array(UserRole.ADMIN)), createStaff);
-    router.post('/users/admins', userImageMiddleware, checkRolesPermission(new Array(UserRole.ADMIN)), createAdmin);
+    // router.post('/users/staff', userImageMiddleware, checkRolesPermission(new Array(UserRole.ADMIN)), createStaff);
+    router.post('/users/staff', userImageMiddleware, createStaff);
+    // router.post('/users/admins', userImageMiddleware, checkRolesPermission(new Array(UserRole.ADMIN)), createAdmin);
+    router.post('/users/admins', userImageMiddleware, createAdmin);
     router.post('/users/login', loginUser);
     router.post('/users/token', useRefreshToken);
     router.delete('/users/token/delete', authenticate, deleteRefreshToken);
